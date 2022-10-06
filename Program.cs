d00a2619f5d600b6b264112b7c5618b9d1eab17c
Christer Carlsson <christer.carlsson@realgymnasiet.se>
2022-10-06 09:30:09 +0200

    Game done, shipit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int hemligtNummer = 47;
+
+System.Console.WriteLine("Gissa på ett tal mellan 1 och 100");
+
+int gissning = int.Parse(Console.ReadLine());
+
+while (gissning != hemligtNummer)
+{
+    if (gissning < hemligtNummer)
+    {
+        Console.WriteLine("Gissa högre");
+    }
+    else if (gissning > hemligtNummer)
+    {
+        Console.WriteLine("Gissa lägre");
+    }
+    gissning = int.Parse(Console.ReadLine());
+
+}
+System.Console.WriteLine("Bra gissat!");
+
